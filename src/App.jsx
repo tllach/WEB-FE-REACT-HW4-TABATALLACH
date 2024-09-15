7c8c42a73f6e8563abb7bf06cc22898c214b27c8
Tabata Llach <tllach@uninorte.edu.co>
2024-09-15 17:10:55 -0500

    header finished

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen font-serif">
       <Header />
-      
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 function Header() {
     return (
-        <header className="w-full flex justify-center items-center h-[80px] bg-[#574dec]">
-            <h1 className="text-3xl text-center text-[#FFFFFF]">
-            Good Old Electronics Devices
+        <header className="w-full flex-row inline-flex justify-center items-center bg-[#1d245a]">
+            <img className="h-auto max-w-12 px-1" src="/src/assets/computer-disc.gif" alt="Logo"></img>
+            <h1 className="font-Handjet text-5xl text-[#FFFFFF] py-5">
+                Good Old Electronics Devices
             </h1>
+            <img className="h-auto max-w-12 px-1" src="/src/assets/computer-disc.gif" alt="Logo"></img>
         </header>
     );
 }
