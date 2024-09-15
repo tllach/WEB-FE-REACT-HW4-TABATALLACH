0496c7482ae74ee9cbea5e54bc1856078d92ee41
Tabata Llach <tllach@uninorte.edu.co>
2024-09-15 18:32:52 -0500

    file with data changed from json to js

---
+++ src/utils/data.js
@@ -0,0 +1,58 @@
+export const SWOTData = [
+        {   "id": 1,
+            "type": "Strengths",
+            "items": [
+                { "id": 1, 
+                "title": "Unique Product Offering:",
+                "description": "Specializing in vintage electronics creates a niche market that attracts collectors and tech enthusiasts."},
+                { "id": 2, 
+                "title": "Sustainability Appeal:",
+                "description": "Selling second-hand items promotes environmental sustainability, appealing to eco-conscious customers."},
+                { "id": 3, 
+                "title": "Nostalgia Factor:",
+                "description": "Many customers may be drawn to vintage products due to sentimental value or a desire to relive past experiences."}
+            ]
+        },
+        {   "id": 2,
+            "type": "Weaknesses",
+            "items": [
+                { "id": 1, 
+                "title": "Limited Stock:",
+                "description": "Relying on old or second-hand products may result in inconsistent stock levels and limited availability of certain items."},
+                { "id": 2, 
+                "title": "Potential for Repairs:",
+                "description": "Vintage electronics may require repairs or maintenance due to age, which can be costly and time-consuming."},
+                { "id": 3, 
+                "title": "Shipping Costs:",
+                "description": "Shipping fragile or heavy items can be expensive, potentially deterring customers from making purchases."}
+            ]
+        },
+        {   "id": 3,
+            "type": "Opportunities",
+            "items": [
+                { "id": 1, 
+                "title": "Growing Interest in Retro Tech:",
+                "description": "There is a rising trend in retro and vintage technology, with an increasing number of collectors and enthusiasts seeking older devices."},
+                { "id": 2, 
+                "title": "Partnerships with Repair Shops:",
+                "description": "Partnering with repair services could offer added value to customers concerned about maintenance or repairs."},
+                { "id": 3, 
+                "title": "International Market:",
+                "description": "By expanding shipping options, the website could tap into global markets where demand for vintage electronics is high."}
+            ]
+        },
+        {   "id": 4,
+            "type": "Threats",
+            "items": [
+                { "id": 1, 
+                "title": "Technological Obsolescence:",
+                "description": "As newer technologies emerge, vintage electronics may become harder to repair, or their functionality may diminish over time."},
+                { "id": 2, 
+                "title": "Product Authenticity Concerns:",
+                "description": "Ensuring that products are genuine and in good working condition can be challenging, potentially leading to dissatisfied customers or negative reviews."},
+                { "id": 3, 
+                "title": "Competitive Market:",
+                "description": "Other sellers, especially on platforms like eBay or Amazon, may offer similar products at competitive prices."}
+            ]
+        }
+    ]
