af45ebb6118a14d7c5c311e727809cc24ca0eb57
Tabata Llach <tllach@uninorte.edu.co>
2024-09-15 14:55:57 -0500

    react vite template init files, and header implement

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      
+    </div>
+  );
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <header className="w-full flex justify-center items-center h-[80px] bg-[#574dec]">
+            <h1 className="text-3xl text-center text-[#FFFFFF]">
+            Good Old Electronics Devices
+            </h1>
+        </header>
+    );
+}
+
+export default Header;
