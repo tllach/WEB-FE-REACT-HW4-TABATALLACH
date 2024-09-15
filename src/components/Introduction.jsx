0732658b1c66dfa21ed0cdea23de9697813189a2
Tabata Llach <tllach@uninorte.edu.co>
2024-09-15 17:41:02 -0500

    main and introduction file added

---
+++ src/components/Introduction.jsx
@@ -0,0 +1,12 @@
+function Introduction(){
+    return (
+        <>
+            <h1 className= 'text-center text-sky-700 text-5xl pt-5'>Welcome!</h1>
+            <p className= 'pt-8'>At Good Old electronics Devices, we believe that a solid strategy is the cornerstone of any successful business, 
+                that&apos;s why this section of our website offers an in-depth look at our strategic framework, including a detailed <b>SWOT</b> analysis 
+                (Strengths, Weaknesses, Opportunities, Threats), as well as our Mision, Vision and Policies and our Strategic Objectives.</p>
+        </>
+    );
+}
+
+export default Introduction;
---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import Introduction from './Introduction';
+
+function Main() {
+  return (
+    <main className="grid-rows-4 px-12 py-2">
+      <Introduction />
+    </main>
+  )
+}
+
+export default Main;
