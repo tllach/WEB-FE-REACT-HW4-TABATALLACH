a7618df4408019daa98c6861ff07eda369174070
Tabata Llach <tllach@uninorte.edu.co>
2024-09-15 22:03:32 -0500

    erase unused line

--- src/components/Diagnosis.jsx
+++ src/components/Diagnosis.jsx
@@ -7,7 +7,6 @@ function Diagnosis() {
             <h1 className="text-3xl py-5">1. Diagnosis (SWOT)</h1>
             <div className="grid grid-cols-2 grid-rows-2 gap-4">
                 {swotData.map((data) => {
-                    console.log("data", data);
                     return (
                         <SwotCard key={data.id} id={data.id} title={data.type} items={data.items} />
                     )
