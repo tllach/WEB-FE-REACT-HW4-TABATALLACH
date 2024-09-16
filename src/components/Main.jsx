a7c4103fe35d8eadcf8b48dd9647701e300c8afc
Tabata Llach <tllach@uninorte.edu.co>
2024-09-16 14:07:17 -0500

    ourIdentity jsx and initial information added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,14 @@
 import Introduction from './Introduction';
 import Diagnosis from './Diagnosis';
 import StrategicObjectives from './StrategicObjectives';
+import OurIdentity from './OurIdentity';
 
 function Main() {
   return (
     <main className="grid-rows-4 px-12 py-2">
       <Introduction />
       <Diagnosis />
+      <OurIdentity />
       <StrategicObjectives />
     </main>
   )
---
+++ src/components/OurIdentity.jsx
@@ -0,0 +1,10 @@
+
+function OurIdentity() {
+    return (
+        <>
+        <h1 className = "text-3xl py-5">2. Our Identity</h1>
+        </>
+    );
+}
+
+export default OurIdentity;
