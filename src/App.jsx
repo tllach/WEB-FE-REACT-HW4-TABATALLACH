d87e93b05b732bb8bd05f33b26c3dc3c4186211b
Tabata Llach <tllach@uninorte.edu.co>
2024-09-16 17:02:15 -0500

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen font-serif">
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer className="w-full flex-row inline-flex justify-center items-center bg-[#1d245a] py-5 mt-5">
+            <p className="font-Handjet text-white text-2xl">&copy; 2024 Tabata Llach Bruges. All rights reserved.</p>
+        </footer>
+    );
+}
+
+export default Footer;
